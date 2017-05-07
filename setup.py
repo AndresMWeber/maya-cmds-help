75a8758d439a433705a362cbd3bae8e1906e28d8
andresmweber <andresmweber@gmail.com>
2017-05-07 01:21:25 -0400

    removed project import from setup.py it was causing installation issues since the project imports before installing in tox

--- setup.py
+++ setup.py
@@ -1,12 +1,9 @@
 """Packaging settings."""
-
 from codecs import open
 from os.path import abspath, dirname, join
 from subprocess import call
-
 from setuptools import Command, find_packages, setup
-
-from maya_signatures import __version__
+__version__ = '0.2.0'
 
 this_dir = abspath(dirname(__file__))
 with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
